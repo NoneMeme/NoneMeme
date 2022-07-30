75460ccff4fed27030695fc8f351c2dd17bf8f5d
yijie4188 <yijie4188@gmail.com>
2022-07-30 20:35:58 +0800

    feat: support link share and click change image

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,26 +1,44 @@
 import config from './config.js'
 
+let cur = NaN
+
+const pathRe = /^meme\/(\d+)\..*/
+const sortedItems = config.items.sort((a, b) => Number(a.replace(pathRe, '$1')) > Number(b.replace(pathRe, '$1')) ? 1 : -1)
+
+const title = document.querySelector('#mainContent > div.header > a.title')
+const memeImg = document.getElementById('memeImg')
+
 function random(max) {
     return ~~(Math.random() * max) + 1
 }
 
-function changePic(img, index, count) {
-    const i = random(count)
-
-    index.ariaBusy = true
-    img.src = `./meme/${i}.jpg`
-    index.innerText = "# " + i
+function setupImg(img) {
+    title.ariaBusy = 'true'
+    title.innerText = `# ${cur}`
+    title.href = `#${cur}`
+    img.src = sortedItems[cur]
     img.onload = () => {
-        index.ariaBusy = false
+        title.ariaBusy = 'false'
     }
     img.onclick = () => {
-        changePic(img, index, count)
+        cur = random(config.count)
+        setupImg(img)
     }
 }
 
-(async () => {
-    const title = document.querySelector('#mainContent > div.header > div.title')
-    const memeImg = document.getElementById('memeImg')
-
+(() => {
     document.querySelector('#desc').innerHTML = `NoneBot 群大佬们的日常，目前已有 ${ config.count } 张。`
+    const hashVal = location.hash.replace(/^#(.*)/, '$1')
+    switch (hashVal) {
+        case '': break
+        case 'gallery':
+            break
+        default:
+            cur = Number(hashVal)
+            break
+    }
+    if (isNaN(cur)) {
+        cur = random(config.count)
+    }
+    setupImg(memeImg)
 })()
