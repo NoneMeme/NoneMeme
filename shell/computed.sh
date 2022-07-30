119fde9576a98ef33fc792f62c6b33c2f2904c89
yijie4188 <yijie4188@gmail.com>
2022-07-31 00:17:11 +0800

    refactor: refactor output file items property struct

--- shell/computed.sh
+++ shell/computed.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 echo 'export default {
   count: '"$(ls ./meme -l |grep "^-"|wc -l)"',
-  items: [ "'"$(echo "$(ls -d meme/*)" | sed ':a;N;$!ba;s/\n/", "/g')"'" ]
+  items: [
+    "'"$(echo "$(ls -d meme/*)" | sed ':a;N;$!ba;s/\n/",\n    "/g')"'"
+  ]
 }' > ./static/scripts/config.js
