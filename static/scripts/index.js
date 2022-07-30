ab776ed9b1b04075c5c46f0e1c1675d71d92403d
yijie4188 <yijie4188@gmail.com>
2022-07-30 18:36:34 +0800

    refactor: support dynamic desc

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,4 +1,4 @@
-import conf from './config.js'
+import config from './config.js'
 
 function random(max) {
     return ~~(Math.random() * max) + 1
@@ -18,71 +18,9 @@ function changePic(img, index, count) {
     }
 }
 
-function _img(src) {
-    const container = document.createElement('div')
-    const img = document.createElement('img')
+(async () => {
+    const title = document.querySelector('#mainContent > div.header > div.title')
+    const memeImg = document.getElementById('memeImg')
 
-    container.classList.add('image')
-    container.style.height = `${conf.gridImageHeight}px`
-
-    const loadingAnim = container.animate([
-        { backgroundColor: 'transparent' },
-        { backgroundColor: '#555' },
-        { backgroundColor: 'transparent' },
-    ], { duration: 1000, iterations: Infinity })
-
-    img.src = src
-    img.style.height = conf.gridImageHeight + 'px'
-    container.appendChild(img)
-    img.style.visibility = 'hidden'
-
-    img.onload = () => {
-        img.style.visibility = 'visible'
-        loadingAnim.cancel()
-    }
-
-    container.onclick = () => {
-        window.open(src)
-    }
-
-    return container
-}
-
-function renderGrid(grid, count) {
-    let images = []
-
-    for (let i = 0; i < count; i++) {
-        images.push(i)
-    }
-
-    images = images
-        .sort(() => (Math.random() - 0.5))
-        .slice(0, conf.maxGridImagesDisplay + 1)
-
-    const frag = document.createDocumentFragment()
-
-    images.forEach(index => {
-        frag.appendChild(_img(`./meme/${index + 1}.jpg`))
-    })
-
-    grid.appendChild(frag)
-}
-
-async function doInit() {
-    const title = document.querySelector('#mainContent > div.header > div.title');
-    const memeImg = document.getElementById('memeImg');
-    const imgGrid = document.getElementById('grid');
-    const fresh = document.querySelector('#gallery > div.header > div.opts > span.material-icons.refresh');
-
-    console.log(fresh)
-
-    fresh.onclick = () => {
-        imgGrid.innerHTML = ''
-        renderGrid(imgGrid, conf.count)
-    }
-
-    changePic(memeImg, title, conf.count)
-    renderGrid(imgGrid, conf.count)
-}
-
-doInit()
+    document.querySelector('#desc').innerHTML = `NoneBot 群大佬们的日常，目前已有 ${ config.count } 张。`
+})()
