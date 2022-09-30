ac14d0c611740c444e4e874cc730d3433c739472
Seidko <hidden2333@163.com>
2022-09-30 13:22:57 +0800

    chore: delete deprecated file

--- shell/computed.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-echo 'export default {
-  count: '"$(ls ./meme -l |grep "^-"|wc -l)"',
-  items: [
-    "'"$(echo "$(ls -d meme/*)" | sed ':a;N;$!ba;s/\n/",\n    "/g')"'"
-  ]
-}' > ./static/scripts/config.js
